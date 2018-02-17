725c0aff66179a0a2296076b884443fc9f4eb671
pipidi <986494553@qq.com>
2018-02-18 00:55:21 +0800

    add html && add test

--- learngit/myproject/myproject/boards/tests.py
+++ learngit/myproject/myproject/boards/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse#Django 2.0 remove django.core.urlresolvers
+from django.urls import resolve
+from .views import home
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    def test_home_view_code(self):  #查看主页的状态码 断言是否是200
+        url=reverse('home')
+        response=self.client.get(url)
+        self.assertEqual(response.status_code,200)
+
+    def test_home_url_resolve_home_view(self):
+        '''
+        Django uses it to match a
+        requested URL with a list
+        of URLs listed in the urls.py
+        module
+        '''
+        view=resolve('/')
+        self.assertEqual(view.func,home)
--- learngit/myproject/myproject/boards/views.py
+++ learngit/myproject/myproject/boards/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Board
 def home(request):
-    return HttpResponse('Hello, World!')
+    # return HttpResponse('Hello, World!')
+    boards=Board.objects.all()#从数据库里面获取所有的版块对象
+    return render(request,'home.html',{'boards':boards})
