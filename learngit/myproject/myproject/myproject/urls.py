6e30b9858cf34abcec9bbed833c734990fd69beb
pipidi <986494553@qq.com>
2018-03-05 17:18:47 +0800

    add change password

--- learngit/myproject/myproject/myproject/urls.py
+++ learngit/myproject/myproject/myproject/urls.py
@@ -21,18 +21,16 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),  #mmp学了这么久才知道第一个参数是正则！
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    url(r'^signup/$',accounts_views.signup, name='signup'),
-    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^logout/$',auth_views.LogoutView.as_view(),name='logout'),#base view
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
-    url(r'^reset/$',auth_views.PasswordResetView.as_view(
-        template_name='password_reset.html',
-        email_template_name='password_reset_email.html',
-        subject_template_name='password_reset_subject.txt'
-    ),
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^reset/$',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html',
+            email_template_name='password_reset_email.html',
+            subject_template_name='password_reset_subject.txt'
+        ),
         name='password_reset'),
     url(r'^reset/done/$',
         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
@@ -43,5 +41,12 @@ urlpatterns = [
     url(r'^reset/complete/$',
         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
         name='password_reset_complete'),
-]
 
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+        name='password_change_done'),
+]
